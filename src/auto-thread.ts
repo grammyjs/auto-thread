7c9943bbf3201aa58c28237b1885dacf0dfff860
KnorpelSenf <shtrog@gmail.com>
2024-02-22 00:53:14 +0100

    fix: only set message_thread_id for same chats

--- src/auto-thread.ts
+++ src/auto-thread.ts
@@ -24,11 +24,20 @@ const METHODS = new Set([
 
 export function autoThread<C extends Context>(): MiddlewareFn<C> {
     return async (ctx, next) => {
+        const chat_id = ctx.chat?.id;
         const message_thread_id = ctx.msg?.message_thread_id;
         const is_topic_message = ctx.msg?.is_topic_message || false;
-        if (message_thread_id !== undefined && is_topic_message) {
+        if (
+            chat_id !== undefined &&
+            message_thread_id !== undefined &&
+            is_topic_message
+        ) {
             ctx.api.config.use(async (prev, method, payload, signal) => {
-                if (!("message_thread_id" in payload) && METHODS.has(method)) {
+                if (
+                    METHODS.has(method) &&
+                    "chat_id" in payload && payload.chat_id === chat_id &&
+                    !("message_thread_id" in payload)
+                ) {
                     Object.assign(payload, { message_thread_id });
                 }
                 return await prev(method, payload, signal);
